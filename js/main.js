47303e0e42c9577610b034b7951c2c9e7e7e8302
quoctri <quoctri200888@gmail.com>
2021-02-25 20:05:20 +0700

    issue

--- js/main.js
+++ js/main.js
@@ -1,8 +1,12 @@
 function fetchIssues() {
+    let colorSeverity = '#333';
+
     const issues = JSON.parse(localStorage.getItem('issues')) || [];
     console.log(issues);
     let issuesList = document.getElementById('issuesList');
+
     issuesList.innerHTML = '';
+
     for (let i = 0; i < issues.length; i++) {
         const id = issues[i].id;
         const desc = issues[i].description;
@@ -10,18 +14,29 @@ function fetchIssues() {
         const assignedTo = issues[i].assignedTo;
         const status = issues[i].status;
 
-
+        switch (severity) {
+            case 'Low':
+                colorSeverity = 'black';
+                break;
+            case 'Medium':
+                colorSeverity = 'green';
+                break;
+            case 'High':
+                colorSeverity = 'red';
+                break;
+            default:
+                break
+        }
 
         issuesList.innerHTML += '<div class="well">' + 'issues ID' + id
             + '<p><span class="label label-info">' + status + '</span></p>'
             + desc
-            + '<p><span class="glyphicon glyphicon-time"></span>' + severity
+            + '<p id="ser" style="color: ' + colorSeverity + ' "><span class="glyphicon glyphicon-time"></span>' + severity
             + '<p><span class="glyphicon glyphicon-user"></span>' + assignedTo + '</p>'
             + '<a href="javascript:;" class="btn btn-warning" onclick="setStatusClosed(\'' + id + '\')">Close</a>'
             + '<a href="javascript:;" class="btn btn-danger" onclick="deleteIssues(\'' + id + '\')">Delete</a>'
         '</div>';
     }
-
 }
 
 function saveIssue(e) {
@@ -31,7 +46,6 @@ function saveIssue(e) {
     const issueSeverity = document.getElementById('issueSeverityInput').value;
     const issueAssignedTo = document.getElementById('issueAssignedToInput').value;
     const issueStatus = 'Open';
-
     const issue = {
         id: issueId,
         description: issueDesc,
@@ -58,9 +72,23 @@ function saveIssue(e) {
 document.getElementById('issueInputForm').addEventListener('submit', saveIssue);
 
 function setStatusClosed(id) {
-    console.log('setStatusClosed', id)
+    const issues = JSON.parse(localStorage.getItem('issues'))
+    for (let i = 0; i < issues.length; i++) {
+        if (issues[i].id === Number(id)) {
+            issues[i].status = "Closed";
+        }
+    }
+    localStorage.setItem('issues', JSON.stringify(issues));
+    fetchIssues();
 }
 
 function deleteIssues(id) {
-    console.log('deleteIssues', id)
+    const issues = JSON.parse(localStorage.getItem('issues'))
+    for (let i = 0; i < issues.length; i++) {
+        if (issues[i].id === Number(id)) {
+            issues.splice(i, 1);
+        }
+    }
+    localStorage.setItem('issues', JSON.stringify(issues));
+    fetchIssues();
 }
